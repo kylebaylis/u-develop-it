2006972704539089ea7101f1d3de42f2a9533d16
Kyle <kylejmbaylis@gmail.com>
2022-05-10 17:10:57 -0400

    updated get routes to show party name

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
+/* candidates must be dropped first, because of foreign key requiring parties to exist */
+
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+/* constraint relies on parties table, so parties table must be defined first */
+
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -8,5 +15,7 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL
-);sour
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
